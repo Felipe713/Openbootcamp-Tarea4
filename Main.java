418c4a7ddd8d5a00cd1b8c7b8f9d5c23b4f1ca00
Felipe Valenzuela <felipeivp@gmail.com>
2022-11-15 20:02:50 -0300

    Parte 1

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
-public class Main {
-    
+import java.util.Scanner;
+
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner numeroIF = new Scanner (System.in);
+        System.out.print("Ingrese un numero: ");
+
+        int n = numeroIF.nextInt();
+
+        if (n > 0 ){
+            System.out.printf("El numero %d es positivo", n);
+        } else if (n < 0) {
+            System.out.printf("El numero %d es negativo", n);
+        } else {
+            System.out.printf("El numero %d es cero", n);
+        }
+    }
+
 }
+
